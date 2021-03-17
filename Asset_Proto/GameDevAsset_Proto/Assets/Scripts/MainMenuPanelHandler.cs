b9b482e9c2e12d0d276870bdb8140edab8068024
George Westergaard <rocketguy124@gmail.com>
2021-03-16 23:28:26 -0400

    Worked on MainMenu and UI assets

---
+++ Asset_Proto/GameDevAsset_Proto/Assets/Scripts/MainMenuPanelHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPanelHandler : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject InstructionsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitchPanel(int panelNum)
+    {
+        switch (panelNum)
+        {
+            case 0:
+                MainMenuPanel.SetActive(true);
+                InstructionsPanel.SetActive(false);
+                break;
+
+            case 1:
+                MainMenuPanel.SetActive(false);
+                InstructionsPanel.SetActive(true);
+                break;
+        }
+    }
+}

09774e134c61de35180763b7ee95b2a7869be19d
George Westergaard <rocketguy124@gmail.com>
2021-03-23 11:23:33 -0400

    Player movement and staff rotation through PlayerController

---
+++ Asset_Proto/GameDevAsset_Proto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 moveInput;
+    public Rigidbody2D theRB;
+
+    [Header("Aiming")]
+    public Transform staffArm;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+
+        theRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+
+
+        //rotation for staff to aim at mouse
+        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        staffArm.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle-90));
+    }
+}

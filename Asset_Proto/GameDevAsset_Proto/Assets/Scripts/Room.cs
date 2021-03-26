e85c9ae30292f6f88f4c0b06e0fbfd43bffa4f8e
George Westergaard <rocketguy124@gmail.com>
2021-03-26 00:06:28 -0400

    Doors working, Rooms close to done

--- Asset_Proto/GameDevAsset_Proto/Assets/Scripts/Room.cs
+++ Asset_Proto/GameDevAsset_Proto/Assets/Scripts/Room.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public bool closeWhenEntered, enemiesCleared;
+
+    public GameObject[] doors;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private bool roomActive;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +20,50 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemies.Count > 0 && roomActive && enemiesCleared)
+        {
+            for(int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+            if(enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+
+                    closeWhenEntered = false;
+                }
+            }
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             CameraController.instance.ChangeCameraTarget(transform);
+
+            if (closeWhenEntered)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
+
+            roomActive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            roomActive = false;
         }
     }
 }

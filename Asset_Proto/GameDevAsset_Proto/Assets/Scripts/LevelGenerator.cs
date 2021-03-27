c4a229034ce4ab3e2bfd51516f4195cf3e429fdd
George Westergaard <rocketguy124@gmail.com>
2021-03-27 13:58:43 -0400

    Starting LevelGeneration

---
+++ Asset_Proto/GameDevAsset_Proto/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject layoutRoom;
+
+    public Color startColor, endColor;
+    //public GameObject startRoom;
+   // public GameObject endRoom;
+
+    public int distanceToExit;
+
+    public Transform generationPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(layoutRoom, generationPoint.position, generationPoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

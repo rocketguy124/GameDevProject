c338057d8fc2630dc28b9d0c81bdf90bb5a63112
George Westergaard <rocketguy124@gmail.com>
2021-04-15 13:15:36 -0400

    Added Credits in Menu and adjusted Controls

--- Asset_Proto/GameDevAsset_Proto/Assets/Scripts/MainMenuPanelHandler.cs
+++ Asset_Proto/GameDevAsset_Proto/Assets/Scripts/MainMenuPanelHandler.cs
@@ -6,6 +6,7 @@ public class MainMenuPanelHandler : MonoBehaviour
 {
     public GameObject MainMenuPanel;
     public GameObject InstructionsPanel;
+    public GameObject InstructionsPanel2;
     public GameObject CreditsPanel;
 
     // Start is called before the first frame update
@@ -26,18 +27,28 @@ public class MainMenuPanelHandler : MonoBehaviour
             case 0:
                 MainMenuPanel.SetActive(true);
                 InstructionsPanel.SetActive(false);
+                InstructionsPanel2.SetActive(false);
                 CreditsPanel.SetActive(false);
                 break;
 
             case 1:
                 MainMenuPanel.SetActive(false);
                 InstructionsPanel.SetActive(true);
+                InstructionsPanel2.SetActive(false);
                 CreditsPanel.SetActive(false);
                 break;
 
             case 2:
                 MainMenuPanel.SetActive(false);
                 InstructionsPanel.SetActive(false);
+                InstructionsPanel2.SetActive(true);
+                CreditsPanel.SetActive(false);
+                break;
+
+            case 3:
+                MainMenuPanel.SetActive(false);
+                InstructionsPanel.SetActive(false);
+                InstructionsPanel2.SetActive(false);
                 CreditsPanel.SetActive(true);
                 break;
 
